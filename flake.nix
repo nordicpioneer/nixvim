e0cb57b0c74da5659ba15870ab64eb6d4b4e88fd
Adrian Helgå Vestøl <a@8v.no>
2025-07-18 14:55:42 +0200

    run nix flake update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "deCort.tech NeoVim configuration";
+  description = "Nordicpioneer's NeoVim configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
