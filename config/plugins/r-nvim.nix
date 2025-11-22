6047662d3cf901d6c35b5ce87e21df61c4d1b185
Adrian Helgå Vestøl <a@8v.no>
2025-11-22 18:56:15 +0100

    add R.nvim options in extraConfigLua

--- config/plugins/r-nvim.nix
+++ config/plugins/r-nvim.nix
@@ -18,9 +18,36 @@
     }
   ];
   extraConfigLua = ''
-    require("r").setup {
-      rconsole_width = 66,
-      rconsole_height = 0
+    -- Extended R.nvim configuration (from docs)
+    ---@type RConfigUserOpts
+    local opts = {
+      hook = {
+        on_filetype = function()
+          -- <CR> sends current line / selection to R
+          vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
+          vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+        end,
+      },
+      R_args = { "--quiet", "--no-save" },
+      min_editor_width = 72,
+      rconsole_width = 78,
+      objbr_mappings = {
+        -- Object browser mappings
+        c = "class",  -- call R functions
+        ["<localleader>gg"] = "head({object}, n = 15)", -- {object} gets replaced
+        v = function()
+          -- Run Lua function: toggle object browser view
+          require("r.browser").toggle_view()
+        end,
+      },
+      disable_cmds = {
+        "RClearConsole",
+        "RCustomStart",
+        "RSPlot",
+        "RSaveClose",
+      },
     }
+    
+    require("r").setup(opts)
   '';
 }
